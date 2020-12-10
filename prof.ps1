bd4847c5e7ff20b689597e3b74de01dfb9f8c797
fate <dhmony-99@hotmail.com>
2020-12-10 12:44:35 +0300

    more funcs

--- prof.ps1
+++ prof.ps1
@@ -1,3 +1,17 @@
+set-location C:\
+$Shell.WindowTitle=”fuxsocy”
+$Shell = $Host.UI.RawUI
+$shell.BackgroundColor = “Black”
+$shell.ForegroundColor = “Gray”
+Clear-Host
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+
+
 function func_nano {
     
     C:\nano\bin\nano.exe
@@ -24,7 +38,19 @@ Set-Alias dsk func_desktop
 
 function rvenv {
 
-    Set-Location "D:\Projects\R.Fate\fate-env\Scripts"
-    powershell.exe -File "activate"
+    D:\Projects\Aesthetic\venv\Scripts\Activate.ps1
 }
-Set-Alias svenv rvenv
+Set-Alias venv rvenv
+
+function _bot {
+
+    Set-Location D:\Projects\Aesthetic\
+}
+Set-Alias src _bot
+
+
+function _run {
+	
+    py -3.8 D:\Projects\Aesthetic\launcher.py
+}
+Set-Alias run _run
