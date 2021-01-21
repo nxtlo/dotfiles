54f5bb9bb57b81592ee96f74c002e0319452fbc7
fate <35225215+nxtlo@users.noreply.github.com>
2021-01-21 13:28:58 +0300

    clean up

--- windows/prof.ps1
+++ windows/prof.ps1
@@ -11,6 +11,18 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 
+
+if ($Prompt1 -eq "Ok") {
+[Microsoft.VisualBasic.Interaction]::MsgBox("Reboot will occur in 5 minutes.", "OkOnly,Information","IT")
+shutdown /r /t 300
+}
+
+if ($Prompt1 -eq "Cancel") {
+[Microsoft.VisualBasic.Interaction]::MsgBox("Reboot has been canceled.", "OKOnly,Information","IT")
+break
+}
+
+
 Set-PSReadlineOption -Color @{
     "Command" = [ConsoleColor]::Green
     "Parameter" = [ConsoleColor]::Gray
